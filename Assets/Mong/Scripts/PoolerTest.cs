90fb3224a19a37c4b4e3ff8c7664a29f8aeba049
정몽현 <wjdahdgus12@naver.com>
2023-02-14 15:44:09 +0900

    fix : 풀매니저 수정

--- Assets/Mong/Scripts/PoolerTest.cs
+++ Assets/Mong/Scripts/PoolerTest.cs
@@ -1,6 +1,7 @@
 using ObjectPool;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EditorTools;
 using UnityEngine;
 
 public class PoolerTest : MonoBehaviour
@@ -9,28 +10,35 @@ public class PoolerTest : MonoBehaviour
     private GameObject[] prefabs;
 
     string Key;
-    string Key4 = "맞는 파티클";
-
 
     public Dictionary<string, GameObject> GetDic = new Dictionary<string, GameObject>();
 
     public PoolManager poolManager;
 
     // Update is called once per frame
-    public void PoolGet(string get)
+    private void PoolAdd()
     {
         Key = "공격사운드";
         GetDic.Add(Key, poolManager.Get(prefabs[0]));
-        Key = "맞는사운드";
+        Key = "피격사운드";
         GetDic.Add(Key, poolManager.Get(prefabs[1]));
         Key = "걸음사운드";
         GetDic.Add(Key, poolManager.Get(prefabs[2]));
         Key = "공격파티클";
         GetDic.Add(Key, poolManager.Get(prefabs[3]));
-        Key = "맞는파티클";
-        GetDic.Add(Key4, poolManager.Get(prefabs[4]));
-
+        Key = "피격파티클";
+        GetDic.Add(Key, poolManager.Get(prefabs[4]));
     }
+    private void PoolGet(string get)
+    {
+        Key = get;
+        poolManager.NameGet(Key);
+    }
+    private void Start()
+    {
+        PoolAdd();
+    }
+
     void Update()
     {
         
@@ -40,11 +48,13 @@ public class PoolerTest : MonoBehaviour
             //poolManager.Get(prefabs[0]);
             //PoolGet(prefabs[0]);
             //GetDic.TryGetValue(Key, out prefabs[0]);
+            PoolGet("피격파티클"); 
         }
         if (Input.GetKey(KeyCode.S))
         {
             //poolManager.Get(prefabs[1]);
             //GetDic.TryGetValue(Key1, out prefabs[1]);
+            PoolGet("AttackParticle");
 
         }
         if (Input.GetKeyDown(KeyCode.D))
@@ -61,16 +71,12 @@ public class PoolerTest : MonoBehaviour
             //GetDic.ContainsKey(Key3);
             //GetDic.TryGetValue(Key3, out prefabs[3]);
 
-
         }
         if (Input.GetKeyDown(KeyCode.G))
         {
             //poolManager.Get(prefabs[4]);
             //GetDic.
-
-            GameObject prefab = poolManager.Get(prefabs[4]);
-            GetDic.TryGetValue(Key4, out prefab);
-
+           
         }
     }
 }
